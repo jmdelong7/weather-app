2d1b682ca7b534ba5349f153ab351aa4e5a81fe9
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-12-31 23:50:36 -0800

    gifConditionsSearch, weatherSearch, searchListener
    Lots of styling as well.
    Core functionality - weatherSearch gets weather data when user submits
    request.
    gifConditionsSearch - takes condition from the search to display a gif.
    searchListener - adds submit listener to the search bar to run when search
    is submitted.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@
 // process.env. ^^^
 import './styles.css';
 
-async function weatherSearch(location) {
+async function fetchWeatherData(location) {
+  const encoded = encodeURIComponent(location);
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${process.env.VISUAL_CROSSING_KEY}`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encoded}?key=${process.env.VISUAL_CROSSING_KEY}`,
     { mode: 'cors' }
   );
   return await response.json();
 }
 
 async function getWeatherData(location) {
-  const data = await weatherSearch(location);
+  const data = await fetchWeatherData(location);
 
   const { resolvedAddress, description, latitude, longitude } = data;
   const locationInfo = { resolvedAddress, description, latitude, longitude };
@@ -69,7 +70,45 @@ async function displayWeatherData(location) {
   currEles.humidityEle.textContent = weatherData.currentForecast.humidity;
   currEles.precipEle.textContent = weatherData.currentForecast.precip;
   currEles.windspeedEle.textContent = weatherData.currentForecast.windspeed;
-  console.log(weatherData);
+
+  const locEles = {
+    resolvedAddress: document.getElementById('location'),
+    description: document.getElementById('description'),
+  };
+
+  locEles.resolvedAddress.textContent =
+    weatherData.locationInfo.resolvedAddress;
+  locEles.description.textContent = weatherData.locationInfo.description;
+
+  return weatherData.currentForecast.conditions;
 }
 
-displayWeatherData('Bozeman');
+async function gifConditionsSearch(conditions) {
+  const encoded = encodeURIComponent(conditions);
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=${process.env.GIPHY_KEY}&s=${encoded}`,
+    { mode: 'cors' }
+  );
+  const jsonData = await response.json();
+  const gif = jsonData.data.images.fixed_width.url;
+  const gifFrame = document.getElementById('gif-frame');
+  gifFrame.style.backgroundImage = `url(${gif})`;
+}
+
+async function weatherSearch(searchInput) {
+  const data = await displayWeatherData(searchInput);
+  gifConditionsSearch(data);
+}
+
+function searchListener() {
+  const searchBar = document.getElementById('search');
+  document.getElementById('searchForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    weatherSearch(searchBar.value);
+  });
+}
+
+searchListener();
+
+// displayWeatherData('san diego');
+// gifConditionsSearch('funny');
